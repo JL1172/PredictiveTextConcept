cc28c90124db0b20afaf33e3d4d64805ad554dea
Jacob Lang <jacoblang72@comcast.net>
2023-08-22 21:06:44 -0400

    wip

--- doggos/src/App.js
+++ doggos/src/App.js
@@ -1,2 +1,22 @@
 import React from 'react';
 import axios from 'axios';
+import Dogs from './Dogs';
+
+
+export default class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            dogs : "",
+        }
+    }
+    render() {
+        
+        return (
+            <div>
+                <h1>Hello Doggos</h1>
+                <Dogs />
+            </div>
+        )
+    }
+}
---
+++ doggos/src/Dogs.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Dog from "./Dog";
+
+export default class Dogs extends React.Component {
+    render() {
+        return (
+            <div>
+                <Dog />
+            </div>
+        )
+    }
+}
